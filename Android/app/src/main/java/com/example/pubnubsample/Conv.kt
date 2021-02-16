2dab281349db29182b732511baee1e283ea94541
Lukasz Klich <klich.lukasz@gmail.com>
2021-02-16 12:52:19 +0100

    Repro

--- Android/app/src/main/java/com/example/pubnubsample/Conv.kt
+++ Android/app/src/main/java/com/example/pubnubsample/Conv.kt
@@ -5,15 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_conv.*
 import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
-class Conv: Activity() {
+class Conv : Activity() {
 
+    @Inject
+    lateinit var pubNubHolder: PubNubHolder
     private val channel: String by lazy { "meta.${(1..1000).random()}" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conv)
-        MainActivity.pubnub.subscribe(channels = listOf(channel))
+        pubNubHolder.pubnub.subscribe(channels = listOf(channel))
         button.setOnClickListener {
             onClick()
         }
@@ -26,6 +30,6 @@ class Conv: Activity() {
 
     override fun onPause() {
         super.onPause()
-        MainActivity.pubnub.unsubscribe(channels = listOf(channel))
+        pubNubHolder.pubnub.unsubscribe(channels = listOf(channel))
     }
 }
--- Android/app/src/main/java/com/example/pubnubsample/MainActivity.kt
+++ Android/app/src/main/java/com/example/pubnubsample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pubnubsample
 
+import android.app.Application
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,72 +13,88 @@ import com.pubnub.api.enums.PNReconnectionPolicy
 import com.pubnub.api.models.consumer.PNStatus
 import com.pubnub.api.models.consumer.pubsub.PNMessageResult
 import com.pubnub.api.models.consumer.pubsub.PNSignalResult
+import dagger.Component
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class MainActivity : AppCompatActivity() {
+@Component
+@Singleton
+interface ApplicationComponent {
+    fun inject(mainActivity: MainActivity)
+    fun inject(conv: Conv)
+}
 
-    companion object {
-        val pubnub: PubNub by lazy { pubnubSetup() }
-        private fun pubnubSetup(): PubNub {
-            val config = PNConfiguration()
-            config.subscribeKey = "sub-c-ec88e64e-b248-11ea-af7b-9a67fd50bac3"
-            config.authKey = ""
-            config.uuid = "oim8"
-            //config.secure = true
-            config.reconnectionPolicy = PNReconnectionPolicy.NONE
-            config.logVerbosity = PNLogVerbosity.BODY
 
-            //config.cipherKey = ""
-            config.maximumConnections = 2
-//        config.heartbeatInterval = 20
-//        config.presenceTimeout = 20
+// appComponent lives in the Application class to share its lifecycle
+class MyApplication : Application() {
+    // Reference to the application graph that is used across the whole app
+    val appComponent = DaggerApplicationComponent.create()
+}
 
-            return PubNub(config).also { Log.e("PubNub", "Created pubnub $it") }
+@Singleton
+class PubNubHolder @Inject constructor() {
+    val pubnub: PubNub by lazy { pubnubSetup() }
+    val listener: SubscribeCallback = object : SubscribeCallback() {
+        override fun status(pubnub: PubNub, pnStatus: PNStatus) {
+            //Thread.sleep(1_000)
+            Log.e("PubNub", pnStatus.toString())
+            Log.e("PubNub", "Thread: ${Thread.currentThread().id.toString()}")
+            //last_status.text = pnStatus.toString()
+        }
+
+        override fun message(pubnub: PubNub, pnMessageResult: PNMessageResult) {
+            Log.e("PubNub", pnMessageResult.toString())
+        }
+
+        override fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult) {
+            Log.e("PubNub", pnSignalResult.toString())
         }
 
     }
-    private var listener: SubscribeCallback? = null
 
+    private fun pubnubSetup(): PubNub {
+        val config = PNConfiguration()
+        config.subscribeKey = "sub-c-ec88e64e-b248-11ea-af7b-9a67fd50bac3"
+        config.authKey = ""
+        config.uuid = "oim8"
+        //config.secure = true
+        config.reconnectionPolicy = PNReconnectionPolicy.NONE
+        config.logVerbosity = PNLogVerbosity.BODY
+
+        //config.cipherKey = ""
+        config.maximumConnections = 1
+//        config.heartbeatInterval = 20
+//        config.presenceTimeout = 20
+        Log.e("PubNub", "Registered listener $listener")
+        val pubnub = PubNub(config).also { Log.e("PubNub", "Created pubnub $it") }
+        pubnub.addListener(listener)
+
+        return pubnub
+    }
+
+}
+
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var pubNubHolder: PubNubHolder
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        listener = object : SubscribeCallback() {
-            override fun status(pubnub: PubNub, pnStatus: PNStatus) {
-                Log.e("PubNub", pnStatus.toString())
-                Log.e("PubNub", "Thread: ${Thread.currentThread().id.toString()}")
-
-                last_status.text = pnStatus.toString()
-            }
-
-            override fun message(pubnub: PubNub, pnMessageResult: PNMessageResult) {
-                Log.e("PubNub", pnMessageResult.toString())
-            }
-
-            override fun signal(pubnub: PubNub, pnSignalResult: PNSignalResult) {
-                Log.e("PubNub", pnSignalResult.toString())
-            }
-
-        }
-        Log.e("PubNub", "Registered listener $listener")
-
-        pubnub.addListener(listener!!)
-
         val group = "cg_1_41033"
-        pubnub.subscribe(channelGroups = listOf(group))
+        pubNubHolder.pubnub.subscribe(channelGroups = listOf(group))
 
         start_test.setOnClickListener {
             subscribeToRandomMeta()
         }
     }
 
-
     private fun subscribeToRandomMeta() {
         val intent = Intent(this, Conv::class.java)
         startActivity(intent)
     }
-
 }
